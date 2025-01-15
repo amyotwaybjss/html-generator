000341719e03f3d1a7e343178473e812df035da7
Amy Otway <amy.otway@bjss.com>
2025-01-15 15:18:43 +0000

    Initial commit

---
+++ makingClass.py
@@ -0,0 +1,9 @@
+# let's work with fruit trees for the example
+# a tree has a species name and a genus name, it also has a group it belongs to
+# a tree may be dormant (inactive), flowering (semi-active) or fruiting (active in use)
+# when fruiting, it has several qualities, such as location, fruit colour, fruit shape...
+# these qualities are optional and may depend on location.
+# potentially each tree has a short description to say what it looks like
+# potentially there are images of the tree which vary depending on status of tree (flower or fruit)
+# orchard owner is a separate object with different attributes
+# aim is to pull up different combinations of these traits in a text block, so that I don't have to write the details
