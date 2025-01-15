1fd50a4bee1ffc51f3c969b21575d92e4015e188
Amy Otway <amy.otway@bjss.com>
2025-01-15 17:17:13 +0000

    Adding location to Tree function

--- treeList.py
+++ treeList.py
@@ -1,9 +1,9 @@
 from definingTrees import Tree
 
 # Creating trees:
-gala = Tree('gala', 'RoYal', 'Apple', 'Fruiting')
-golden = Tree('delicious', 'golden', 'Apple', 'Fruiting')
-w_grape = Tree('grape', 'white', 'Grape', 'Fruiting')
+gala = Tree('gala', 'RoYal', 'Apple', 'Fruiting', 'Plot 1')
+golden = Tree('delicious', 'golden', 'Apple', 'Fruiting', 'Plot 1')
+w_grape = Tree('grape', 'white', 'Grape', 'Fruiting', 'Plot 2')
 r_grape = Tree('grape', 'red', 'Grape', 'flowering')
 orange = Tree('orange', None, 'orange', 'flowering')
 
