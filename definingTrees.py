d6b39087cc7cf991ba704134f9eadcc74ee4cb87
Amy Otway <amy.otway@bjss.com>
2025-01-15 16:47:38 +0000

    Accounting for Null genus

--- definingTrees.py
+++ definingTrees.py
@@ -1,21 +1,26 @@
 class Tree:
 
+    # adding instances allows us to track all trees
+    tree_list = []
     tree_statuses = {'dormant', 'flowering', 'fruiting'}
 
     # adding __slots__ prevents new attributes being added
     def __init__(self, species, genus, group, status):
-        self.species = species.title()
-        self.genus = genus.title()
+        # note that in the init statement, adding = 'something' gives a default of 'something'
+        # this makes the attribute optional
+        self.species = species
+        self.genus = genus
         if status.lower() not in self.tree_statuses:
             raise ValueError(f'Invalid status')
-        self.status = status.lower()
-        self.group = group.upper()
+        self.status = status
+        self.group = group
         # note that in the init statement, adding = 'something' gives a default of 'something'
+        # this makes the attribute optional
+        Tree.tree_list.append(self)
 
     def __str__(self):
-        return f'{self.group}: {self.species}, {self.genus}. Currently {self.status}.'
+        if self.genus is None:
+            return f'{self.group.upper()}: {self.species.title()}. Currently {self.status.lower()}.'
+        else:
+            return f'{self.group.upper()}: {self.species.title()}, {self.genus}. Currently {self.status.lower()}.'
 
-
-gala = Tree('Gala', 'RoYal', 'Apple', 'Flowering')
-
-print(gala)
