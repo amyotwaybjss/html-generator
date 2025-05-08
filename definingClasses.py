222d51c9ce5deb1d8d6eb8cfddc8ebb9f08f5839
amyotwaybjss <amy.otway@bjss.com>
2025-05-08 17:04:18 +0100

    Adding Field class

--- definingClasses.py
+++ definingClasses.py
@@ -11,13 +11,11 @@ class Tree:
         self.id = (str(species) + str(genus) + str(group)).lower()
         self.species = species
         self.genus = genus
+        self.group = group
         if status.lower() not in self.tree_statuses:
             raise ValueError(f'Unknown status')
         self.status = status
-        self.group = group
-        if status.lower() == 'fruiting' and location is None:
-            raise ValueError(f'Fruiting Tree must have location')
-        self.location = location
+        self.fields = set()
         Tree.tree_list.append(self)
 
     def __str__(self):
@@ -25,5 +23,24 @@ class Tree:
         return (f'{self.group.upper()}: '
                 f'{self.species.title()}'
                 f'{'' if self.genus is None else ', ' + str(self.genus).title()}. '
-                f'Currently {self.status.lower()}{'' if self.location is None else ' at ' + str(self.location).upper()}. '
+                f'Currently {self.status.lower()}. '
+                # f'Currently {self.status.lower()}{'' if self.location is None else ' at ' + str(self.location).upper()}. '
                 f'ID: {self.id}')
+
+    def add_field(self, field):
+        self.fields.add(field)
+        field.trees.add(self)
+
+
+class Field:
+    def __init__(self, name):
+        self.name = name
+        self.trees = set() # trees in field
+
+    def add_tree(self, tree):
+        self.trees.add(tree)
+        tree.fields.add(self)
+
+    def __str__(self):
+        return f'Field: {self.name}'
+
--- treeDirectory.py
+++ treeDirectory.py
@@ -1,4 +1,4 @@
-from definingTreeClass import Tree
+from definingClasses import Tree, Field
 
 # Creating trees:
 gala = Tree('gala', 'RoYal', 'Apple', 'Fruiting', 'Plot 1')
@@ -12,4 +12,16 @@ orange = Tree('orange', None, 'orange', 'flowering')
 all_trees = []
 for item in Tree.tree_list:
     all_trees.append(item)
+
+# Creating fields:
+field_a = Field("Field A")
+field_b = Field("Field B")
+field_c = Field("Field C")
+
+# Add trees to fields
+field_a.add_tree(gala)
+field_b.add_tree(gala)
+field_b.add_tree(golden)
+
+print(f"{golden.species} is in fields: {[f.name for f in golden.fields]}")
 
